f4c694134f8dd8fe293b9b88884a0ec3c5dfe251
Pricecito <matias.gutierrez.1254@gmail.com>
2025-07-14 19:13:09 -0500

    Creacion de modelo

---
+++ src/main/java/com/personalblog/persistence/model/Article.java
@@ -0,0 +1,12 @@
+package com.personalblog.persistence.model;
+
+import java.time.LocalDate;
+
+public class Article {
+
+    private int id;
+    private String title;
+    private String content;
+    private LocalDate publishedDate;
+
+}

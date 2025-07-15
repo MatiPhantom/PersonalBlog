cc32948fa7827b54d5dc6ab21045068fbf7b1f76
Pricecito <matias.gutierrez.1254@gmail.com>
2025-07-14 20:13:21 -0500

    Controller

--- src/main/java/com/personalblog/controller/BlogController.java
+++ src/main/java/com/personalblog/controller/BlogController.java
@@ -1,12 +1,18 @@
 package com.personalblog.controller;
 
 
+import com.personalblog.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 @Controller
 public class BlogController {
+    @Autowired
+    private ArticleService articleService;
 
-    public String index(){
+    public String index(Model model){
+
         return "blog/index";
     }
 }

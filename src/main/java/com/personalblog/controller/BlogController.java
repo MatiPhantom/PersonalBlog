9cbbf2893edafa4cc39fbec7d14933a6af0a25d7
Pricecito <matias.gutierrez.1254@gmail.com>
2025-07-15 19:30:42 -0500

    Implementando css html en el blog

--- src/main/java/com/personalblog/controller/BlogController.java
+++ src/main/java/com/personalblog/controller/BlogController.java
@@ -5,12 +5,16 @@ import com.personalblog.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/blog")
 public class BlogController {
     @Autowired
     private ArticleService articleService;
 
+    @GetMapping
     public String index(Model model){
         model.addAttribute("articles", articleService.getAllArticles());
         return "blog/index";

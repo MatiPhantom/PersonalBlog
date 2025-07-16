79cc6757a486162e88bd2d6cef6692768a10aecb
Pricecito <matias.gutierrez.1254@gmail.com>
2025-07-15 20:11:18 -0500

    Contruyendo Controlador

--- src/main/java/com/personalblog/controller/ArticleController.java
+++ src/main/java/com/personalblog/controller/ArticleController.java
@@ -1,4 +1,27 @@
 package com.personalblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.personalblog.service.ArticleService;
+
+
+@Controller
+@RequestMapping("/article")
 public class ArticleController {
+
+    @Autowired
+    private ArticleService articleService;
+
+    @GetMapping("/{id}")
+    public String index(@PathVariable("id") int id, Model model) {
+        model.addAttribute("article", articleService.getArticleById(id));
+        return "articleDetails/index";
+    }
+    
 }

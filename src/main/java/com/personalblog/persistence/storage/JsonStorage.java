92c82116d0ac1ba55269e38ce4f7c32eb38c50b0
Pricecito <matias.gutierrez.1254@gmail.com>
2025-07-16 22:23:33 -0500

    Cargando data

--- src/main/java/com/personalblog/persistence/storage/JsonStorage.java
+++ src/main/java/com/personalblog/persistence/storage/JsonStorage.java
@@ -1,10 +1,14 @@
 package com.personalblog.persistence.storage;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.personalblog.persistence.model.Article;
 
@@ -16,14 +20,17 @@ public class JsonStorage {
 
     private static final ObjectMapper mapper = new ObjectMapper();
 
-    private static List<Article> articles;
+    private static List<Article> articles=new ArrayList<>();
 
     public static List<Article> getArticles() {
         return articles;
     }
 
-    public static void save(){
+    public static void setArticle(Article article) {
+        articles.add(article);
+    }
 
+    public static void save(){
         try(FileWriter writer = new FileWriter("articles.json")) {
             mapper.writeValue(writer, articles);
             writer.flush();
@@ -33,6 +40,24 @@ public class JsonStorage {
             log.error("Error saving articles to JSON file", e);
         }
 
+    }
+
+    public static List<Article> load(){
+        File file = new File("articles.json");
+        if(file.exists()){
+            try{
+                FileReader reader = new FileReader(file);
+                log.info("Articles loaded from JSON file successfully.");
+                return mapper.readValue(reader,new TypeReference<List<Article>>(){});
+            }catch(Exception e){
+                log.error("Error loading articles from JSON file", e);
+                return new ArrayList<>();
+            }
+        }else{
+            return new ArrayList<>();
+        }
+
+        
     }
 
 }
